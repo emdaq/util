0adb7f62bb22eab1e416020d178d6637353c4919
Matt Dean <matt.thomas.dean@gmail.com>
2014-01-21 21:15:05 -0500

    add example test

---
+++ Emdaq.ExampleDataAccess.Tests/EmailRepoTests.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Dapper;
+using Emdaq.DataAccess;
+using Emdaq.ExampleDataAccess.Model;
+using Emdaq.ExampleDataAccess.Repository;
+using NUnit.Framework;
+
+namespace Emdaq.ExampleDataAccess.Tests
+{
+    [TestFixture]
+    public class EmailRepoTests
+    {
+        private SupervisedContext _conn;
+        private EmailRepo _repo;
+
+        private Email _testEmail;
+        private int _entityId;
+
+        [SetUp]
+        public void Setup()
+        {
+            var supervisor = new DataSupervisor();
+            _repo = new EmailRepo(supervisor, ConnFactory.I.OpenEmdaqUnitTestConn);
+
+            _conn = supervisor.ConfirmConnection();
+
+            _entityId = 1;
+            _testEmail = new Email
+            {
+                EmailAddress = "cool@gmail.com",
+                ContactStatusId = ContactStatus.Bounced,
+                ContactTypeId = ContactType.Home,
+                EntityId = _entityId
+            };
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _repo.DeleteEmail(_testEmail);
+            _conn.Dispose();
+        }
+
+        [Test]
+        public void TestUpsertGetAndDeleteEmail()
+        {
+            // initial insert works, and getAll
+            _repo.UpsertEmails(new[] { _testEmail });
+            var fromDb = _repo.GetEmails().FirstOrDefault(x => x.EmailAddress.Equals(_testEmail.EmailAddress) && x.EntityId == _entityId);
+            TestUtility.I.AssertEqual(_testEmail, fromDb);
+
+            // update works, and getAll
+            _testEmail.ContactStatusId = ContactStatus.SpamComplaint;
+            _testEmail.IsPreferred = !_testEmail.IsPreferred;
+            _repo.UpsertEmails(new[] { _testEmail });
+            fromDb = _repo.GetEmails(new[] { "blahthisisntrealandwontbeinthedbblah@fake.com", _testEmail.EmailAddress }).FirstOrDefault(x => x.EntityId == _entityId);
+            TestUtility.I.AssertEqual(_testEmail, fromDb);
+
+            // delete works, and getAll
+            _repo.DeleteEmail(_testEmail);
+            fromDb = _repo.GetEmails(new[] { _testEmail.EmailAddress }).FirstOrDefault(x => x.EntityId == _entityId);
+            Assert.IsNull(fromDb);
+        }
+
+        [Test]
+        public void TestGetEmailsForEntity()
+        {
+            var emails = new List<Email>
+                {
+                    new Email
+                        {
+                            EmailAddress = "cool1@gmail.com",
+                            ContactStatusId = ContactStatus.Bounced,
+                            ContactTypeId = ContactType.Home,
+                            EntityId = _entityId
+                        },
+                    new Email
+                        {
+                            EmailAddress = "cool2@gmail.com",
+                            ContactStatusId = ContactStatus.SpamComplaint,
+                            ContactTypeId = ContactType.Mobile,
+                            EntityId = _entityId
+                        },
+                    new Email
+                        {
+                            EmailAddress = "cool3@gmail.com",
+                            ContactStatusId = ContactStatus.Invalid,
+                            ContactTypeId = ContactType.Work,
+                            EntityId = _entityId
+                        }
+                };
+
+            #region setup
+            _repo.UpsertEmails(emails);
+            #endregion
+
+            var forEntityById = _repo.GetEmailsForEntity(_entityId).ToDictionary(x => x.EmailAddress);
+            Assert.IsTrue(forEntityById.Count == emails.Count);
+            foreach (var email in emails)
+            {
+                TestUtility.I.AssertEqual(email, forEntityById[email.EmailAddress]);
+            }
+
+            var forEntities = _repo.GetEmailsForEntities(new[] { _entityId });
+            Assert.IsTrue(forEntities.ContainsKey(_entityId));
+            Assert.IsTrue(forEntities[_entityId].Count == emails.Count);
+
+            #region teardown
+            using (var conn = ConnFactory.I.OpenEmdaqUnitTestConn())
+            {
+                foreach (var email in emails)
+                {
+                    conn.Execute("DELETE FROM Email WHERE EmailAddress = @emailAddress;", email);
+                }
+            }
+            #endregion
+        }
+    }
+}
---
+++ Emdaq.ExampleDataAccess.Tests/TestUtility.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace Emdaq.ExampleDataAccess.Tests
+{
+    public class TestUtility
+    {
+        #region singleton
+
+        private static readonly Lazy<TestUtility> Singleton = new Lazy<TestUtility>(() => new TestUtility());
+        public static TestUtility I { get { return Singleton.Value; } }
+
+        #endregion
+
+        public void AssertEqual<T>(T o1, T o2, bool lenientDates = true) where T : class
+        {
+            if (o1 == null && o2 == null)
+            {
+                return;
+            }
+
+            Assert.IsNotNull(o1);
+            Assert.IsNotNull(o2);
+
+            foreach (var prop in typeof(T).GetProperties().Where(x => x.PropertyType.IsValueType || x.PropertyType == typeof(string)))
+            {
+                var o1Val = prop.GetValue(o1, null);
+                var o2Val = prop.GetValue(o2, null);
+
+                if (lenientDates && prop.PropertyType == typeof(DateTime))
+                {
+                    // mysql rounds to the nearest second, so let's just make sure dates are within 3 seconds of each other
+                    var diff = ((DateTime)o1Val).Subtract((DateTime)o2Val);
+                    Assert.IsTrue(Math.Abs(diff.TotalSeconds) < 3);
+                }
+                else
+                {
+                    Assert.IsTrue(Equals(o1Val, o2Val));
+                }
+            }
+        }
+
+        public void AsserListEquivalent<T>(IEnumerable<T> l1, IEnumerable<T> l2, Func<T, T, bool> equality = null)
+        {
+            if (equality == null)
+            {
+                CollectionAssert.AreEquivalent(l1, l2);
+            }
+            else
+            {
+                if (l1 == null && l2 == null)
+                {
+                    return;
+                }
+
+                Assert.IsNotNull(l1);
+                Assert.IsNotNull(l2);
+
+                Assert.That(l1, new CollectionEquivalentConstraint(l2).Using<T>((x, y) => equality(x, y) ? 0 : 1));
+            }
+        }
+    }
+}
